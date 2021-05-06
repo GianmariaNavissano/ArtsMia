5f2967d3e8f0fd3183a698bbfa67f9e6ff9ebcea
gianmarianavissano <gianmarianavissano@192.168.1.83>
2021-05-06 17:14:22 +0200

    primo commit

--- src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
+++ src/main/java/it/polito/tdp/artsmia/db/ArtsmiaDAO.java
@@ -6,33 +6,81 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.artsmia.model.ArtObject;
 
 public class ArtsmiaDAO {
 
-	public List<ArtObject> listObjects() {
+	public void listObjects(Map<Integer, ArtObject> map) {
 		
 		String sql = "SELECT * from objects";
-		List<ArtObject> result = new ArrayList<>();
 		Connection conn = DBConnect.getConnection();
 
 		try {
 			PreparedStatement st = conn.prepareStatement(sql);
 			ResultSet res = st.executeQuery();
 			while (res.next()) {
-
-				ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
-						res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
-						res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
-						res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
 				
-				result.add(artObj);
+				if(!map.containsKey(res.getInt("object_id"))) {
+					ArtObject artObj = new ArtObject(res.getInt("object_id"), res.getString("classification"), res.getString("continent"), 
+							res.getString("country"), res.getInt("curator_approved"), res.getString("dated"), res.getString("department"), 
+							res.getString("medium"), res.getString("nationality"), res.getString("object_name"), res.getInt("restricted"), 
+							res.getString("rights_type"), res.getString("role"), res.getString("room"), res.getString("style"), res.getString("title"));
+					
+					map.put(artObj.getId(), artObj);
+				}
+				
+				
+			}
+			conn.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int getPeso(ArtObject a1, ArtObject a2) {
+		String sql = "SELECT count(*) as peso FROM exhibition_objects e1, "+
+				"exhibition_objects e2 WHERE e1.exhibition_id = e2.exhibition_id "+
+				"AND e1.object_id = ? AND e2.object_id = ?";
+		Connection conn = DBConnect.getConnection();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, a1.getId());
+			st.setInt(2, a2.getId());
+			ResultSet rs = st.executeQuery();
+			int peso = 0;
+			if(rs.next()) {
+				peso = rs.getInt("peso");
+			}
+			conn.close();
+			return peso;
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	public List<Adiacenza> getAdiacenze() {
+		String sql ="SELECT e1.object_id, e2.object_id, count(*) as peso "
+				+ "FROM exhibition_objects e1, exhibition_objects e2 "
+				+ "WHERE e1.exhibition_id = e2.exhibition_id AND e1.object_id > e2.object_id "
+				+ "GROUP BY e1.object_id, e2.object_id";
+		Connection conn = DBConnect.getConnection();
+		List<Adiacenza> result = new ArrayList<Adiacenza>();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				result.add(new Adiacenza(rs.getInt("id1"), rs.getInt("id2"), rs.getInt("peso")));
 			}
 			conn.close();
 			return result;
 			
-		} catch (SQLException e) {
+		} catch(SQLException e) {
 			e.printStackTrace();
 			return null;
 		}
--- src/main/java/it/polito/tdp/artsmia/model/Model.java
+++ src/main/java/it/polito/tdp/artsmia/model/Model.java
@@ -1,5 +1,66 @@
 package it.polito.tdp.artsmia.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.artsmia.db.Adiacenza;
+import it.polito.tdp.artsmia.db.ArtsmiaDAO;
+
 public class Model {
 
+	private Graph<ArtObject, DefaultWeightedEdge> grafo;
+	private ArtsmiaDAO dao;
+	private Map<Integer, ArtObject> idMap; //identity map per evitare di chiamare il dao più volte inutilmente
+	
+	public Model() {
+		dao = new ArtsmiaDAO();
+		idMap = new HashMap<Integer, ArtObject>();
+	}
+	
+	public void creaGrafo() {
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		//Aggiunta vertici
+		dao.listObjects(idMap);
+		Graphs.addAllVertices(grafo, idMap.values());
+		
+		//Aggiunta archi
+		//Approccio 1
+		//doppio for per confrontare ogni coppia di vertici se va collegata
+		//ci mette circa 600 giorni
+		/*for(ArtObject a1 : this.grafo.vertexSet()) {
+			for(ArtObject a2 : this.grafo.vertexSet()) {
+				//Verifico che i vertifi siano diversi e non vi sia già un arco tra loro
+				if(!a1.equals(a2) && !this.grafo.containsEdge(a1,a2)) {
+					//devo collegare a1 ad a2?
+					 int peso = dao.getPeso(a1, a2);
+					 if(peso > 0) {
+						 Graphs.addEdge(this.grafo, a1, a2, peso);
+					 }
+				}
+			}
+		}*/
+		
+		//Approccio 2
+		//per tutti i vertici (uno alla volta) chiedo al db di dirmi quali sono i vertici a quello collegati
+		//è ancora troppo lento (circa 30 min)
+		
+		//Approccio 3
+		//chiedo al db tutte le coppie di vertici collegati con relativo peso (unica query)
+		List<Adiacenza> adiacenze = this.dao.getAdiacenze();
+		for(Adiacenza a : adiacenze) {
+			Graphs.addEdge(this.grafo, idMap.get(a.getId1()), idMap.get(a.getId2()), a.getPeso());
+		}
+		
+		System.out.println("GRAFO CREATO!");
+		System.out.println("N. di vertici: "+grafo.vertexSet().size());
+		System.out.println("N. di archi: "+grafo.edgeSet().size());
+	}
+	
 }
